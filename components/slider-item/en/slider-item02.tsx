b4a6e105ab3ee4859908d27379ca1c11668df059
가동재 <masterforce999@naver.com>
2026-03-12 13:19:29 +0900

    업데이트

--- components/slider-item/en/slider-item02.tsx
+++ components/slider-item/en/slider-item02.tsx
@@ -19,23 +19,23 @@ export default function SliderItem02En() {
         sizes="100vw"
         alt="WYD 2027 DID Main Image 2"
       />
-      <div className="text-white heading01b absolute top-[30dvh] z-40 pl-12 max-[767px]:pl-5 pr-4 flex flex-col gap-y-2 w-full max-w-[1400px] left-1/2 -translate-x-1/2">
-        <div className="h-[clamp(30px,9dvw,90px)] overflow-hidden">
+      <div className="text-white heading01b absolute top-[30dvh] z-40 pl-12 max-[767px]:pl-5 pr-4 flex flex-col gap-y-1 w-full max-w-[1400px] left-1/2 -translate-x-1/2">
+        <div className="h-[clamp(36px,9dvw,90px)] overflow-hidden">
           <SlideText style={{ animationDelay: "1.2s" }} className="text-[clamp(20px,6dvw,60px)]">
             Take courage.
           </SlideText>
         </div>
-        <div className="h-[clamp(30px,9dvw,90px)] overflow-hidden">
+        <div className="h-[clamp(36px,9dvw,90px)] overflow-hidden">
           <SlideText style={{ animationDelay: "1.8s" }} className="text-[clamp(20px,6dvw,60px)]">
             I have conquered
           </SlideText>
         </div>
-        <div className="h-[clamp(30px,9dvw,90px)] overflow-hidden">
+        <div className="h-[clamp(36px,9dvw,90px)] overflow-hidden">
           <SlideText style={{ animationDelay: "2.2s" }} className="text-[clamp(20px,6dvw,60px)]">
             the world.
           </SlideText>
         </div>
-        <div className="h-[clamp(28px,7.5dvw,81px)] overflow-hidden">
+        <div className="h-[clamp(30px,7.5dvw,81px)] overflow-hidden">
           <SlideSmallText style={{ animationDelay: "2.7s" }} className="text-[clamp(16px,3dvw,36px)]">
             (John 16:33)
           </SlideSmallText>
