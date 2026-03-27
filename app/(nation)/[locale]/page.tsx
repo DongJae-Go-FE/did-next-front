e62940927d9c7311b9d08f7c506b07e0f5ae3694
가동재 <masterforce999@naver.com>
2026-03-27 14:57:17 +0900

    업데이트

--- app/(nation)/[locale]/page.tsx
+++ app/(nation)/[locale]/page.tsx
@@ -33,7 +33,7 @@ export default async function Home({
       <div className="relative bg-black">
         <MainSlider locale={locale} />
       </div>
-      <FightPage locale={locale} />
+      {/* <FightPage locale={locale} /> */}
 
       <div
         className="w-full min-h-[80dvh] max-[767px]:min-h-0 bg-white px-12 py-24 max-[1079px]:px-8 max-[1079px]:py-16 max-[767px]:px-5 max-[767px]:py-8"
@@ -43,31 +43,31 @@ export default async function Home({
           <IntroPage locale={locale} />
         </div>
       </div>
-      <ApplyCtaSection locale={locale} />
-      <div
+      {/* <ApplyCtaSection locale={locale} /> */}
+      {/* <div
         className="w-full bg-white px-12 py-24 max-[1079px]:px-8 max-[1079px]:py-16 max-[767px]:px-5 max-[767px]:py-12"
         id="map"
       >
         <div className="max-w-[1400px] mx-auto">
           <MapPage locale={locale} />
         </div>
-      </div>
-      <div
+      </div> */}
+      {/* <div
         className="w-full h-fit bg-gray-100 px-12 py-24 max-[1079px]:px-8 max-[1079px]:py-16 max-[767px]:px-5 max-[767px]:py-12 scroll-mt-20"
         id="sponsor"
       >
         <div className="max-w-[1400px] mx-auto">
           <SponsorPage locale={locale} />
         </div>
-      </div>
-      <div
+      </div> */}
+      {/* <div
         className="w-full h-[70dvh] max-[1079px]:h-auto bg-white px-12 py-24 max-[1079px]:px-8 max-[1079px]:py-16 max-[767px]:px-5 max-[767px]:py-12 bg-fixed bg-center bg-cover relative flex justify-center items-center gap-y-8 flex-col"
         style={{ backgroundImage: `url('${IMAGE_BASE}/did/main/main05.jpeg')` }}
       >
         <div className="max-w-[1400px] mx-auto w-full flex flex-col items-center gap-y-8">
           <PrayPage locale={locale} />
         </div>
-      </div>
+      </div> */}
     </main>
   );
 }
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -120,7 +120,7 @@ export default function Header({ locale = "kr" }: { locale?: Locale }) {
             </Select>
           </li>
 
-          <li
+          {/* <li
             className={cn(
               "transition-colors duration-500",
               shouldShowWhiteBg
@@ -129,7 +129,7 @@ export default function Header({ locale = "kr" }: { locale?: Locale }) {
             )}
           >
             <Menu locale={locale} />
-          </li>
+          </li> */}
         </ul>
       </div>
     </header>
