8233df0cc7a078e1e16f061b35dea4e6dde98d94
가동재 <masterforce999@naver.com>
2026-04-04 22:44:06 +0900

    업데이트

--- components/main-slider.tsx
+++ components/main-slider.tsx
@@ -19,12 +19,12 @@ import { Pause, Play, ChevronLeft, ChevronRight } from "lucide-react";
 import "@egjs/flicking/dist/flicking.css";
 import "@egjs/flicking-plugins/dist/pagination.css";
 
-//import SliderItem00 from "./slider-item/slider-item00";
-import SliderItem000 from "./slider-item/slider-item000";
+import SliderItem00 from "./slider-item/slider-item00";
+//import SliderItem000 from "./slider-item/slider-item000";
 import SliderItem01 from "./slider-item/slider-item01";
 import SliderItem02 from "./slider-item/slider-item02";
-//import SliderItem00En from "./slider-item/en/slider-item00";
-import SliderItem000En from "./slider-item/en/slider-item000";
+import SliderItem00En from "./slider-item/en/slider-item00";
+//import SliderItem000En from "./slider-item/en/slider-item000";
 import SliderItem01En from "./slider-item/en/slider-item01";
 import SliderItem02En from "./slider-item/en/slider-item02";
 
@@ -148,12 +148,12 @@ export default function MainSlider({ locale = "kr" }: { locale?: Locale }) {
       <div className="flicking-camera">
         {isKr ? (
           <>
-            {/* <Panel>
-              <SliderItem00 />
-            </Panel> */}
             <Panel>
-              <SliderItem000 />
+              <SliderItem00 />
             </Panel>
+            {/* <Panel>
+              <SliderItem000 />
+            </Panel> */}
             <Panel>
               <SliderItem01 />
             </Panel>
@@ -163,12 +163,12 @@ export default function MainSlider({ locale = "kr" }: { locale?: Locale }) {
           </>
         ) : (
           <>
-            {/* <Panel>
-              <SliderItem00En />
-            </Panel> */}
             <Panel>
-              <SliderItem000En />
+              <SliderItem00En />
             </Panel>
+            {/* <Panel>
+              <SliderItem000En />
+            </Panel> */}
             <Panel>
               <SliderItem01En />
             </Panel>
