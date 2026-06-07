7fb7b73cf2df95f1da2367d0fe2ab65d6c5eab31
가동재 <masterforce999@naver.com>
2026-06-07 19:10:22 +0900

    메타그래트 업데이트

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -4,7 +4,7 @@ import { ImageResponse } from "next/og";
 
 // 루트 세그먼트에 두면 모든 하위 라우트에 og:image 가 자동 주입됩니다.
 // 카카오/네이버 등은 SVG og:image 를 지원하지 않으므로
-// public/logo.svg 를 ImageResponse 로 PNG 렌더링해서 내보냅니다.
+// public/logo.svg 를 그대로 PNG 로 변환해서 내보냅니다.
 export const alt = "WYD2027 서울 세계청년대회 교구대회 DID 공식 홈페이지";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
@@ -20,26 +20,13 @@ export default async function OpengraphImage() {
           width: "100%",
           height: "100%",
           display: "flex",
-          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
           background: "#ffffff",
-          fontFamily: "sans-serif",
         }}
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={logoSrc} width={334} height={380} alt="" />
-        <div
-          style={{
-            fontSize: 28,
-            letterSpacing: 6,
-            marginTop: 52,
-            color: "#0047BB",
-            fontWeight: 700,
-          }}
-        >
-          WORLD YOUTH DAY 2027 SEOUL · DAYS IN DIOCESE
-        </div>
+        <img src={logoSrc} width={467} height={530} alt="" />
       </div>
     ),
     { ...size }
